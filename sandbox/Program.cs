105f5f149250ab451f1d5a9a328e0a28fd9632b7
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-02-12 20:14:46 +0900

    feat: disallow multiline expressions

--- sandbox/Program.cs
+++ sandbox/Program.cs
@@ -1,15 +1,21 @@
-// all 3 errors
-int[] values = [11, 22, 33, 44];
+// ERROR: all errors
+int[] values = [
+        1, 2, 3, 4
+];
 
 #pragma warning disable CEK001  // Collection expressions are disallowed
 
-// 4 or more elements but less than 12 chars
+// ERROR: 4 or more elements
 values = [1, 2, 3, 4];
 
-// only an element but more than 12 chars
+// ERROR: only an element but more than 12 chars
 values = [123_456_789];
 
-// OK: only 3 elements and less than 12 chars
+// ERROR: multiline expression is not allowed
+values = [1,
+2];
+
+// OK: only 3 elements and expression is less than or equal to 12 chars
 values = [1, 2, 3];
 
 Console.WriteLine(values.Length);
